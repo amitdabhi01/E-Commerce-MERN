ac07a4c24c75263efa884cde2e408202aee5e247
Dabhi Amit <amitdabhi526@gmail.com>
2026-05-18 18:59:23 +0530

    create login page and their route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import NavbarComponent from "../Components/NavbarComponent";
-import Home from "../Pages/Home";
+
+import Home from "../Pages/Home.jsx";
+import Login from "../Pages/Login.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <NavbarComponent />
-      
+
       <Routes>
         <Route path="/" element={<Home />}></Route>
+
+        <Route path="/login" element={<Login/>}></Route>
 
         <Route path="/login"></Route>
       </Routes>
